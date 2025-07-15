e9c1742a2565e60e70e471b7e2fb0f9d028d6918
izzibizz <contact@izabellind.com>
2025-07-15 14:18:10 +0200

    link video

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -24,7 +24,7 @@ export const LandingPage: React.FC = () => {
         playsInline
         className="fixed top-1/2 left-1/2 w-screen h-screen object-cover transform -translate-x-1/2 -translate-y-1/2 animate-fadeIn"
       >
-        <source src={isTablet ? "https://res.cloudinary.com/dctpyfz60/video/upload/v1752573282/grund-2023-josefine-ostlund-mobile_rnd8cj.mp4" : isMobile? "https://res.cloudinary.com/dctpyfz60/video/upload/v1752575112/grund-2023-josefine-ostlund-mobile_emmga5.mp4" : "https://res.cloudinary.com/dctpyfz60/video/upload/v1752575885/josefine-ostlund-grund-2023-wide_zko61q.mp4"} type="video/mp4" />
+        <source src={isTablet ? "https://res.cloudinary.com/dctpyfz60/video/upload/v1752573282/grund-2023-josefine-ostlund-mobile_rnd8cj.mp4" : isMobile? "https://res.cloudinary.com/dctpyfz60/video/upload/v1752581841/grund-2023-josefine-ostlund-mobile_lrllyl.mp4" : "https://res.cloudinary.com/dctpyfz60/video/upload/v1752575885/josefine-ostlund-grund-2023-wide_zko61q.mp4"} type="video/mp4" />
       </video>
      <img src="/Josefine-ostlund-w-50.svg" alt="josefine östlund" className="absolute z-20 bottom-25 tablet:bottom-2 right-1 max-w-[98%] laptop:max-w-[40%] laptop:bottom-5 laptop:right-5"/>
     </section>
