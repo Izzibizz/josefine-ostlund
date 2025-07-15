8b6f18a4edc2335f342989f47c48f5a6647d6beb
izzibizz <contact@izabellind.com>
2025-07-15 13:53:09 +0200

    background color body

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -26,7 +26,7 @@ export const LandingPage: React.FC = () => {
       >
         <source src={isTablet ? "https://res.cloudinary.com/dctpyfz60/video/upload/v1752573282/grund-2023-josefine-ostlund-mobile_rnd8cj.mp4" : isMobile? "https://res.cloudinary.com/dctpyfz60/video/upload/v1752575112/grund-2023-josefine-ostlund-mobile_emmga5.mp4" : "https://res.cloudinary.com/dctpyfz60/video/upload/v1752575885/josefine-ostlund-grund-2023-wide_zko61q.mp4"} type="video/mp4" />
       </video>
-     <img src="/Josefine-ostlund-w-50.svg" alt="josefine östlund" className="absolute z-25 bottom-20 tablet:bottom-2 right-1 max-w-[98%] laptop:max-w-[40%] laptop:bottom-5 laptop:right-5"/>
+     <img src="/Josefine-ostlund-w-50.svg" alt="josefine östlund" className="absolute z-28 bottom-20 tablet:bottom-2 right-1 max-w-[98%] laptop:max-w-[40%] laptop:bottom-5 laptop:right-5"/>
     </section>
   )
 }
